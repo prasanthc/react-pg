aa76222d9851abf139c39cf1e70d9741d5f3d71e
unknown <prasanth.c@lyncspace.com>
2022-03-20 22:03:44 +0530

    Initial commit to dev branch

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,6 @@ export function Home() {
 
   // const login = () => { authService.login('/'); }
   const logout = () => { return alert("logout"); }
-
   const userText = <div><p>You are signed in!</p><button onClick={ logout }>Logout</button></div>
 
   return <div className="page-home"><h1>Welcome to Books with Hooks</h1>{ userText }</div>;
